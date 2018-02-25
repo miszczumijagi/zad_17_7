829ff753c72c53e5d276bfa07868af2c2d20587c
Kamil Stangret <kamilstangret@wp.pl>
2018-02-25 20:45:26 +0100

    Add all files with content

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+'GOOGLE_CLIENT_ID': '285427494106-9kj8spm6199rtope6gskehf4v013dvii.apps.googleusercontent.com',
+'GOOGLE_CLIENT_SECRET': 'XuC3PjkR5P-gtNNkKBaOeGfU',
+'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
